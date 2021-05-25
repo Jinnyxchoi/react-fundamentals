8fbb5ba0708b3bdd5abaab71dc93042bfb7d3296
jin young <jinnyxchoi@gmail.com>
2021-05-24 20:07:01 -0400

    keys

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,24 +4,34 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const usernameRef = React.useRef()
+  // const usernameRef = React.useRef()
+  const [username, setUsername] = React.useState('')
 
   function handleSubmit(event) {
     event.preventDefault()
     // the preventDefault prevents a full page refresh?
     const value = event.target.elements.usernameInput.value
     //the usernameInput is coming from the id in the input tag.
-    const usernameValue = usernameRef.current.value
-    console.log('usernameValue', usernameValue)
+    // const usernameValue = usernameRef.current.value
     //just another way to access the input
     onSubmitUsername(value)
+  }
+  function handleChange(event) {
+    const {value} = event.target
+    setUsername(value.toLowerCase())
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="usernameInput">Username:</label>
-        <input ref={usernameRef} id="usernameInput" type="text" />
+        <input
+          onChange={handleChange}
+          value={username}
+          // ref={usernameRef}
+          id="usernameInput"
+          type="text"
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
